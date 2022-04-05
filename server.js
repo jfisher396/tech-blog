ce049b253c83307c19e66fe8110b2d8e1bf0671a
James Fisher <jfisher396@hotmail.com>
2022-04-04 21:11:48 -0500

    server working

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const allRoutes = require('./controllers');
+const allRoutes = require("./controllers");
 
-const db = require('./models');
+const db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', allRoutes);
+app.use("/", allRoutes);
 
 db.sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => {console.log(`App listening on PORT ${PORT}`)})
-})
+  app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+});
