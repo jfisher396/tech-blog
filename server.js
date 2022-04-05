c04686668f72a6583bc985908e59399d96192d85
James Fisher <jfisher396@hotmail.com>
2022-04-04 21:06:04 -0500

    added db related folders and setup server.js file

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const allRoutes = require('./controllers');
+
+const db = require('./models');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/', allRoutes);
+
+db.sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => {console.log(`App listening on PORT ${PORT}`)})
+})
