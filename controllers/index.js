78025a37797750fb2014666f2ea06a06197b8f81
James Fisher <jfisher396@hotmail.com>
2022-04-05 19:50:32 -0500

    bcrypt installed and added to User model

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const userRoutes = require('./api/user')
+const userRoutes = require('./api/user-routes')
 
 router.get("/", (req, res) => {
   res.send("This is the get all route");
 });
 
-router.use("/api/users", userRoutes)
+router.use("/api/users", userRoutes);
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { DataTypes, Model } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(userpw) {
+    return bcrypt.compareSync(userpw, this.password);
+  }
+}
 
 User.init(
   {
@@ -36,11 +41,13 @@ User.init(
       beforeCreate: async (newUserData) => {
         // Takes the user's email address and makes all letters lower case before adding it to the database.
         newUserData.email = await newUserData.email.toLowerCase();
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
       // Here, we use the beforeUpdate hook to make all of the characters lower case in an updated email address, before updating the database.
       beforeUpdate: async (updatedUserData) => {
         updatedUserData.email = await updatedUserData.email.toLowerCase();
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
       },
     },
