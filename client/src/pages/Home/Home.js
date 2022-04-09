32a3b6aa21026c63e1f39c8502830152875d625c
James Fisher <jfisher396@hotmail.com>
2022-04-09 13:15:58 -0500

    added proxy to client package.json and if process statement to server

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { Component } from "react"
+import axios from 'axios'
 
-export default function Home() {
-  return (
-    <div>Home</div>
-  )
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+      this.postsAPI();
+  }
+
+  postsAPI(){
+      axios.get("/api/posts").then((res) => {
+          console.log(res.data)
+      }).catch((err) => console.log(err))
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Homepage</h1>
+      </>
+    );
+  }
 }
--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const allRoutes = require("./controllers");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.use("/", allRoutes);
 
 sequelize.sync({ force: false }).then(() => {
