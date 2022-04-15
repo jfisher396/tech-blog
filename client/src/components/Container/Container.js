feae3a42cac19d690fb73f245cd306f56c72b9dc
James Fisher <jfisher396@hotmail.com>
2022-04-15 16:18:08 -0500

    card content updated

--- client/src/components/Container/Container.js
+++ client/src/components/Container/Container.js
@@ -15,7 +15,12 @@ function Container(props) {
               </header>
               <div className="card-content">
                 <p>{post.postBody}</p>
-                <p>Posted by: {array[index].user.username} on {post.createdAt.slice(0,10)} at {post.createdAt.slice(11,16)}</p>
+                
+              </div>
+              <div className="card-footer">
+                <div className="card-footer-item">
+                  <p>Posted by: {array[index].user.username} on {post.createdAt.slice(0,10)} at {post.createdAt.slice(11,16)}</p>
+                </div>
               </div>
               
             </div>
