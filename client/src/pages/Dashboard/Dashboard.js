428bd06a6e494eeda84bbd5d69665c16eb7e0413
James Fisher <jfisher396@hotmail.com>
2022-04-24 15:45:50 -0500

    renders add post button and form when clicked

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -4,39 +4,71 @@ import Card from "../../components/Card/Card";
 import API from "../../utils/API";
 
 class Dashboard extends Component {
-
-  state= {
-    userPosts: []
-  }
+  state = {
+    userPosts: [],
+    newPostFormBoolean: false,
+  };
 
   componentDidMount() {
     API.getUserPosts().then((res) => {
-      
-      this.setState({ userPosts: res.data })
-    })
+      this.setState({ userPosts: res.data });
+    });
   }
 
-
+  showNewPostFormButton = (e) => {
+    e.preventDefault();
+    this.setState({ newPostFormBoolean: true });
+  };
 
   render() {
-    console.log(this.state.userPosts)
-    return (
-    <Container>
-      <h1>Dashboard</h1>
-      {this.state.userPosts.map((post, index, array) => (
-        <Card
-          key={post.id}
-          id={post.id}
-          postTitle={post.title}
-          postBody={post.postBody}
-          // postCreator={array[index].user.username}
-          postCreatedDate={post.createdAt.slice(0, 10)}
-          postCreatedTime={post.createdAt.slice(11, 16)}
-        />
-      )) }
+    // console.log(this.state.userPosts)
 
-    </Container>
-    )
+    return (
+      <Container>
+        <h1>Dashboard</h1>
+        <button
+          type="submit"
+          className="button is-success"
+          onClick={this.showNewPostFormButton}
+        >
+          New Post
+        </button>
+
+        {this.state.newPostFormBoolean && (
+          <form className="field">
+            <label className="label">Title</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                name="post-title"
+                placeholder="Enter post title"
+              />
+              <label class="label">Message</label>
+              <div className="control">
+                <textarea
+                  className="textarea"
+                  name="post-body"
+                  placeholder="Enter your post here"
+                ></textarea>
+              </div>
+            </div>
+          </form>
+        )}
+
+        {this.state.userPosts.map((post, index, array) => (
+          <Card
+            key={post.id}
+            id={post.id}
+            postTitle={post.title}
+            postBody={post.postBody}
+            // postCreator={array[index].user.username}
+            postCreatedDate={post.createdAt.slice(0, 10)}
+            postCreatedTime={post.createdAt.slice(11, 16)}
+          />
+        ))}
+      </Container>
+    );
   }
 }
 
